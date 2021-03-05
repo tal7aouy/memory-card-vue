229b36cf3a5c93d3d4b2955dad9da33717d6b42d
talhaouycoding <talhaouycoding@gmail.com>
2021-03-05 23:26:10 +0100

    user may restart game

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
         />
   </section>
   <h2>{{status}}</h2>
-  <button @click="shuffleCards">shuffle</button>
+  <button @click="restartGame">Restart Game</button>
 </template>
 
 <script>
@@ -42,15 +42,27 @@ export default {
     return remainingCards / 2
   })
 
-const shuffleCards = () => {
-  cardList.value = _.shuffle(cardList.value)
-}
+  const shuffleCards = () => {
+    cardList.value = _.shuffle(cardList.value)
+  }
+  const restartGame = () =>{
+    shuffleCards()
+    cardList.value = cardList.value.map((card,index) =>{
+      return {
+        ...card,
+        matched: false,
+        visible: false,
+        position: index
+
+      }
+    })
+  }
 
 
     for(let i = 0;i<16;i++){
       cardList.value.push({
-        value: i,
-        visible: true,
+        value: 8,
+        visible: false,
         position: i,
         matched: false
       })
@@ -72,11 +84,9 @@ const shuffleCards = () => {
       const cardOne = currentValue[0]
       const cardTwo = currentValue[1]
       if(cardOne.faceValue === cardTwo.faceValue){
-        status.value = "Matched!"
         cardList.value[cardOne.position].matched = true
         cardList.value[cardTwo.position].matched = true
       }else{
-        status.value = "Mismatch!"
         cardList.value[cardOne.position].visible = false
         cardList.value[cardTwo.position].visible = false
       }
@@ -93,7 +103,8 @@ const shuffleCards = () => {
       userSelection,
       status,
       remainingPairs,
-      shuffleCards
+      shuffleCards,
+      restartGame
     }
   }
 
